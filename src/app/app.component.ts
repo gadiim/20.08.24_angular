d57ba79fd70ea1ef4e98378f64e3579f631f9713
Hennadii Melnyk <162840010+gadiim@users.noreply.github.com>
2024-08-22 01:21:35 +0300

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EventItemComponent } from './event-item/event-item.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, EventItemComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
