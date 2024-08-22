4cbc7164f7e4c2b9e3010bcb31d2c4157a61d6e9
Hennadii Melnyk <162840010+gadiim@users.noreply.github.com>
2024-08-22 12:37:21 +0300

    task 1 completed

--- src/app/event-item/event-item.component.ts
+++ src/app/event-item/event-item.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-event-item',
@@ -8,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrl: './event-item.component.css'
 })
 export class EventItemComponent {
-
+  @Input() event: string = 'Nuclear Bomb Explosion in Boston';
+  @Input() date: string = 'October 23, 207';
+  @Input() description: string = 'This event marked the beginning of the Great War,' +
+    'a conflict that lasted only two hours but changed the world forever.' +
+    'During the war, numerous nuclear missiles were launched from both sides,' +
+    'between the U.S. and China. One of the targets was Boston, which was devastated' +
+    'by the blast, leaving the city in ruins and creating the Wasteland.';
 }
